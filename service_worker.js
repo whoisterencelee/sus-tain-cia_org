e2f8008ed0404bef62503fb9c924cf9539e65327
whoisterencelee <whoisterencelee@hotmail.com>
2020-03-18 17:26:13 +0800

    added service worker inorder to get installation

---
+++ service_worker.js
@@ -0,0 +1,34 @@
+self.addEventListener('install', function(e) {
+	e.waitUntil(
+		caches.open('sustaincia.org').then(function(cache) {
+			return cache.addAll([ '/' , 'index.html' ]);
+		})
+	   );
+});
+
+self.addEventListener('fetch', function(event) { // needed for home screen installation https://developers.google.com/web/fundamentals/app-install-banners/
+	console.log(event.request.url);
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+//			console.log("cache match for "+event.request.url)
+			return response || fetch(event.request);
+		})
+	);
+});
+
+/*
+self.addEventListener( 'push' , ( event ) => {
+	let data = event.data.json()
+	//let title = ( event.data && event.data.text() ) || "Message received"
+	let title = data.title || "notification received"
+	let message = data.message || "message received"
+
+	event.waitUntil(
+		self.registration.showNotification( title , {
+			icon : "icons/tube.png" ,
+			body : message ,
+			tag : "pwa" 
+			} )
+	)
+})
+*/
