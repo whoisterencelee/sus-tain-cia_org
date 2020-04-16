5afd3ff765ee63a2d844fa0cd26e7b3f7fa3b901
whoisterencelee <whoisterencelee@hotmail.com>
2020-04-16 20:25:59 +0800

    tap for link

--- service_worker.js
+++ service_worker.js
@@ -21,14 +21,16 @@ self.addEventListener('fetch', function(event) { // needed for home screen insta
 self.addEventListener( 'push' , ( event ) => {
 		let data = event.data.json()
 		//let title = ( event.data && event.data.text() ) || "Message received"
-		let title = data.title || "notification received"
-		let message = data.message || "message received"
+		let note = {
+			icon : "icon.png" ,
+			body : data.message || "message received" ,
+			tag : "pwa" ,
+		}
+		if( data.url ){
+			note.data = { url : data.url }
+		}
 
 		event.waitUntil(
-			self.registration.showNotification( title , {
-				icon : "icon.png" ,
-				body : message ,
-				tag : "pwa"
-			} )
-	       )
+			self.registration.showNotification( data.title || "notification recieved" , note )
+		)
 })
