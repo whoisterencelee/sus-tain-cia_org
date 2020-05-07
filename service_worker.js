c9daa7b6c85a6753e4522e1586188226c03f79ab
whoisterencelee <whoisterencelee@hotmail.com>
2020-05-07 16:00:12 +0800

    make service_worker domain agnostic

--- service_worker.js
+++ service_worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', function(e) {
 	self.skipWaiting()  // update service worker immediately not waiting for tabs to close
 	e.waitUntil(
-		caches.open('sustaincia.org').then(function(cache) {
+		caches.open(location.host).then(function(cache) {
 			return 
 			return cache.addAll([ '/' , 'index.html' ]);
 		})
@@ -19,28 +19,29 @@ self.addEventListener('fetch', function(event) { // needed for home screen insta
 });
 
 self.addEventListener( 'push' , ( event ) => {
-		let data = event.data.json()
-		//let title = ( event.data && event.data.text() ) || "Message received"
-		let note = {
-			icon : "icon.png" ,
-			body : data.message || "message received" ,
-			tag : "pwa" ,
-			requireInteraction : true ,
-		}
-		if( data.url ){
-			note.data = { url : data.url }
-		}
+	let data = event.data.json()
+	//let title = ( event.data && event.data.text() ) || "Message received"
+	let note = {
+		icon : "icon.png" ,
+		body : data.message || "message received" ,
+		tag : "pwa" ,
+		requireInteraction : true ,
+	}
+	if( data.url ){
+		note.data = { url : data.url }
+	}
+	// save message to inbox
 
-		event.waitUntil(
-			self.registration.showNotification( data.title || "notification recieved" , note )
-		)
+	event.waitUntil(
+		self.registration.showNotification( data.title || "notification received" , note )
+	)
 })
 
 // https://stackoverflow.com/questions/38713685/how-can-i-initiate-a-pwa-progressive-webapp-open-from-a-click-on-a-push-notifi
 self.addEventListener( 'notificationclick' , ( event ) => {
 	event.notification.close()
 	var data = event.notification.data
-	var rooturl = new URL('./', location).href
+	//if( !data.url ) data.url = new URL('./', location).href // use if don't have data.url and want to open the app on click
 	if( data.url ){
 		event.waitUntil( clients.matchAll({
 			type: 'window'
